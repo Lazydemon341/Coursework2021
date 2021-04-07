9f771781ae8cd01f02875498af64b09ff7a4e5cf
Alexander <vlas.s.341@gmail.com>
2021-04-07 15:01:02 +0300

    Initial commit

---
+++ app/src/main/java/com/avvlas/coursework2021/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.avvlas.coursework2021.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.avvlas.coursework2021.R
+
+class MainActivity : FragmentActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
